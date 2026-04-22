dfd6e6a1cf65e77384e9fc0ccbf6823356f1b652
Theo Ephraim <theo@dmno.dev>
2026-04-21 23:02:54 -0700

    README and docs cleanup for npm publishing
    - Use absolute GitHub URLs for images and doc links (works on npmjs.com)
    - Add prepack script to copy root README into bumpy package
    - Add note about blank bump file descriptions in docs
    - Remove outdated llms.md (will regenerate when docs site is ready)

--- packages/bumpy/src/types.ts
+++ packages/bumpy/src/types.ts
@@ -158,7 +158,7 @@ export const DEFAULT_CONFIG: BumpyConfig = {
     preamble: [
       `<a href="https://bumpy.varlock.dev"><img src="https://raw.githubusercontent.com/dmno-dev/bumpy/main/images/frog-talking.png" alt="bumpy-frog" width="60" align="left" style="image-rendering: pixelated;" title="Hi! I'm bumpy!" /></a>`,
       '',
-      `This PR was created and will be kept in sync by [bumpy](https://bumpy.varlock.dev) based on your .bumpy bump files. Merge it when you are ready to release the packages listed below:`,
+      `This PR was created and will be kept in sync by [bumpy](https://bumpy.varlock.dev) based on your bump files (in \`.bumpy/\`). Merge it when you are ready to release the packages listed below:`,
       '<br clear="left" />',
     ].join('\n'),
   },
